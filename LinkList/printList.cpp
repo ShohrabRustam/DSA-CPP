c41a52dab547b2412d2b012bd2b113a49bc2445f
Shohrab Rustam <mohdrustam001@gmail.com>
2022-02-07 11:11:12 +0530

    print the list function

---
+++ LinkList/printList.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+public : int data ;
+public : Node * next;
+public : Node(int val ){
+    this->data=val;
+    this->next = NULL;
+}
+};
+class LinkList
+{
+    public : Node * head = NULL;
+    void printList(Node * n){
+        Node *temp = this->head;
+        if(this->head==NULL)
+        {
+            cout <<"The link List is Empty ! "<<endl;
+        }
+        else{
+            while(temp->next!=NULL)
+            {
+                if(temp==head){
+                    cout<<temp->data;
+                }
+                else{
+                cout <<"->"<<temp->data;
+                }
+                temp= temp->next;
+            }
+            cout <<endl;
+        }
+    }
+};
